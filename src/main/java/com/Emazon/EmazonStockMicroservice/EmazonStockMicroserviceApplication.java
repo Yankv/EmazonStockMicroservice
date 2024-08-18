ab7cec7339dea624891ca4346beb16ed53cf7477
Yan Carlos Villegas <yancarlosvillegas7@gmail.com>
2024-08-18 01:37:28 -0500

    Primer commit
    Commit para establecer el repositorio

---
+++ src/main/java/com/Emazon/EmazonStockMicroservice/EmazonStockMicroserviceApplication.java
@@ -0,0 +1,13 @@
+package com.Emazon.EmazonStockMicroservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmazonStockMicroserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmazonStockMicroserviceApplication.class, args);
+	}
+
+}
